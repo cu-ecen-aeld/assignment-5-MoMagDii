f9ee0aa2cb8b47f56887b9b494035c508d6a6b9e
MoMagDii <mohameddbardana@gmail.com>
2023-05-28 05:03:25 +0300

    Fix cross_compile

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,15 +6,15 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5b64ff06e6880614088e0b55e1949d269938a7fb'
+AESD_ASSIGNMENTS_VERSION = '9c2b312c31b24b59f78329b0622b85fe2a40dce2'
 
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-MoMagDii.git'
-AESD_ASSIGNMENTS_SITE_METHOD = git
-AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+AESD_ASSIGNMENTS_SITE='git@github.com:cu-ecen-aeld/assignments-3-and-later-MoMagDii.git'
+AESD_ASSIGNMENTS_SITE_METHOD=git
+AESD_ASSIGNMENTS_GIT_SUBMODULES=YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
